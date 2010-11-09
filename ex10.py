afec2f96539a11a9ad2a7d13c903b54577752200
Phillip Jones <pjones@adaptiveflight.com>
2010-11-08 22:46:12 -0500

    exercise 10

---
+++ ex10.py
@@ -0,0 +1,19 @@
+# Python the Hard Way
+# Exercise 10: What was that?
+
+tabby = "\tI'm tabbed in."
+persion = "I'm split\non a line."
+backslash = "I'm \\ a \\ cat."
+
+fat = """
+I'll do a list:
+\t* Cat food
+\t* fishies
+\t* catnip\n\t* Grass
+"""
+
+print tabby
+print persion
+print backslash
+print fat
+
