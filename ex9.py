830da3d408a68075ba67698d77969d314c51f5e6
Phillip Jones <pjones@adaptiveflight.com>
2010-11-08 22:42:46 -0500

    exercise 9

---
+++ ex9.py
@@ -0,0 +1,15 @@
+# Python the Hard Way
+# Exercise 9: Printing, Printing, Printing
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "here are the days: ", days
+print "here are the months: ", months
+
+print """
+there's something going on here
+with the three double-quotes.
+we'll be able to type as much as want.
+"""
+
