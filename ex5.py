69e8c4b8fd0adee3a57a90fc3bcae4691c936f06
Phillip Jones <pjones@adaptiveflight.com>
2010-11-08 22:22:27 -0500

    extra credit 5

---
+++ ex5.py
@@ -0,0 +1,20 @@
+# Python the Hard Way
+# Excercise 5
+
+Name = 'Zed A. Shaw'
+Age = 35
+Height = 74 # inches
+Weight = 180 # lbs
+Eyes = 'Blue'
+Teeth = 'White'
+Hair = 'Brown'
+
+print "Let's talk about %s." % Name
+print "He's %d inches tall." % Height
+print "He's %d pounds heavy." % Weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (Eyes, Hair)
+print "His teeth are usually %s depending on the coffee." % Teeth
+
+print "If I add %d, %d, and %d I get %d." % (Age, Height, Weight, Age + Height + Weight)
+
