e4f645f8e937a27700581d7fca74bf474e889415
Phillip Jones <pjones13@gmail.com>
2010-11-09 11:46:39 -0500

    exercise 16

--- ex15.py
+++ ex15.py
@@ -8,6 +8,7 @@ txt = open(filename)
 
 print "here is your file %r:" % filename
 print txt.read()
+txt.close()
 
 print "I'll also ask you to type it again:"
 file_again = raw_input("> ")
@@ -16,3 +17,4 @@ txt_again = open(file_again)
 txt_again = open(file_again)
 
 print txt_again.read()
+txt_again.close()
