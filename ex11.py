6cb327a9d227131bc86909435a25caab06cdc00e
Phillip Jones <pjones@adaptiveflight.com>
2010-11-08 22:52:30 -0500

    exercise 11

---
+++ ex11.py
@@ -0,0 +1,15 @@
+# Python the Hard Way
+# Exercise 11: Asking Questions
+
+print "how old are you?",
+age = raw_input()
+
+print "how tall are you?",
+height = raw_input()
+
+print "how much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (age, weight, height)
+
+
